44f25228f06ecfe1afb283908749a45b4303c051
FreelimbOO <JengLCC@gmail.com>
2026-06-10 00:49:43 -0700

    Fix REDIRECT_URI to always use /login path (prevents redirect_uri mismatch when cached OAuth app was registered from a different pathname)

--- vite.config.js
+++ vite.config.js
@@ -64,7 +64,7 @@ logger.warn = (msg, options) => {
 };
 
 // https://vitejs.dev/config/
-// build: 2026-06-10n
+// build: 2026-06-10o
 export default defineConfig({
   customLogger: logger,
   base: '/',
